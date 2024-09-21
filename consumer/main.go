7ff1ce329bb6a3258cccd49796944f6cb214ab88
Nicoleta Voinea <nicoletavoinea17@yahoo.com>
2024-09-21 23:07:18 +0300

    Initial implementation for consumer and producer.

--- consumer/main.go
+++ consumer/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type StatusCode int
+
+const (
+	RECEIVED   StatusCode = iota //0
+	PROCESSING                   //1
+	DONE                         //2
+)
+
+type task struct {
+	TaskId             int32      `json:"id"` //to make it static
+	TaskType           int8       `json:"type"`
+	TaskValue          int8       `json:"value"`
+	TaskState          StatusCode `json:"state"`
+	TaskCreationTime   int64      `json:"creationtime"`
+	TaskLastUpdateTime int64      `json:"lastupdatetime"`
+}
+
+func handleTask(w http.ResponseWriter, r *http.Request) {
+	var receivedTask task
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+		return
+	}
+
+	err = json.Unmarshal(body, &receivedTask)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("Received task: %+v", receivedTask)
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(receivedTask)
+}
 
 func main() {
-	fmt.Println("Initial commit for Consumer service")
+	router := mux.NewRouter()
+	router.HandleFunc("/task", handleTask).Methods("POST")
+
+	log.Println("Starting the Task Consumer on :8081...")
+	err := http.ListenAndServe(":8081", router)
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
--- producer/main.go
+++ producer/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand/v2"
+	"net/http"
 	"time"
+	//	"github.com/prometheus/client_golang/prometheus"
+	//	"github.com/prometheus/client_golang/prometheus/promauto"
+	//"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type StatusCode int
@@ -15,31 +22,57 @@ const (
 )
 
 type task struct {
-	taskId             int32 //to make it static
-	taskType           int8
-	taskValue          int8
-	taskState          StatusCode
-	taskCreationTime   int64
-	taskLastUpdateTime int64
+	TaskId             int32      `json:"id"` //to make it static
+	TaskType           int8       `json:"type"`
+	TaskValue          int8       `json:"value"`
+	TaskState          StatusCode `json:"state"`
+	TaskCreationTime   int64      `json:"creationtime"`
+	TaskLastUpdateTime int64      `json:"lastupdatetime"`
 }
 
 func generateRandomTask() task {
 	now := time.Now()
 
 	var randomTask task
-	randomTask.taskId = rand.Int32()
-	randomTask.taskType = int8(rand.IntN(9))
-	randomTask.taskValue = int8(rand.IntN(99))
-	randomTask.taskState = RECEIVED
-	randomTask.taskCreationTime = now.Unix() //secons elapsed since 1970
-	randomTask.taskLastUpdateTime = randomTask.taskCreationTime
+	randomTask.TaskId = rand.Int32()
+	randomTask.TaskType = int8(rand.IntN(9))
+	randomTask.TaskValue = int8(rand.IntN(99))
+	randomTask.TaskState = RECEIVED
+	randomTask.TaskCreationTime = now.Unix() //secons elapsed since 1970
+	randomTask.TaskLastUpdateTime = randomTask.TaskCreationTime
 	return randomTask
 }
 
+func sendTask(taskToSend task) {
+	jsonTask, err := json.Marshal(taskToSend)
+	if err != nil {
+		log.Fatalf("Failed during Marshal function: %v", err)
+	}
+
+	response, err := http.Post("http://localhost:8081/task", "application/json", bytes.NewBuffer(jsonTask))
+	if err != nil {
+		log.Fatalf("Failed to post task: %v", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		log.Printf("Failed to send task to consumer: received status code %d", response.StatusCode)
+	} else {
+		log.Printf("Task sent to consumer: %+v", taskToSend)
+	}
+}
+
 func main() {
-	fmt.Println("Initial commit for Producer service")
+	//http.Handle("/metrics", promhttp.Handler())
+	//http.ListenAndServe(":2112", nil)
 
 	fmt.Println(generateRandomTask())
 	fmt.Println(generateRandomTask())
 	fmt.Println(generateRandomTask())
+	tosend := generateRandomTask()
+	sendTask(tosend)
+	tosend = generateRandomTask()
+	sendTask(tosend)
+	tosend = generateRandomTask()
+	sendTask(tosend)
 }
